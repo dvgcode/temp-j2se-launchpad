220a8add406a9ed4aa6131518c660067dec21fd9
dvgcode <pdvg.cdac@gmail.com>
2020-12-12 22:34:36 +0530

    First commit with ignore unwanted .settings and .project

---
+++ EclipseMavenCore/src/main/java/com/dvg/launchpad/EclipseMavenCore/App.java
@@ -0,0 +1,13 @@
+package com.dvg.launchpad.EclipseMavenCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
