5c751f614be6410757707305ce39ee8164cdc9e6
dvgcode <pdvg.cdac@gmail.com>
2020-12-13 16:49:26 +0530

    First commiting Maven source code in git

---
+++ src/main/java/com/dvg/launchpad/EclipseMavenCoreGIT/App.java
@@ -0,0 +1,13 @@
+package com.dvg.launchpad.EclipseMavenCoreGIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
